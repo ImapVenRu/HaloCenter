bdce568a1843bf8742289dc6ad1b620a6020f465
ImapVenRu <imap.venru@gmail.com>
2023-04-07 00:19:58 +0700

    Updates

---
+++ precache-manifest.f2d57fc6c266da743368c7cfa020aca2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d683dd7ad0e42597c46",
+    "url": "/HaloCenter/static/css/main.52b71d19.chunk.css"
+  },
+  {
+    "revision": "3d683dd7ad0e42597c46",
+    "url": "/HaloCenter/static/js/main.3d683dd7.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/HaloCenter/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/HaloCenter/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "d5e2a52d9fb93ca1f821",
+    "url": "/HaloCenter/static/js/runtime~main.d5e2a52d.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/HaloCenter/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "1dfbdbbad0f29dfdc904036a6bd9d6aa",
+    "url": "/HaloCenter/index.html"
+  }
+];
